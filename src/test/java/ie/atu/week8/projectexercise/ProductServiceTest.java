5a59dff0543ca2b3c5a423fae4641a4c480fdb87
Gavin <G00402296@atu.ie>
2024-11-25 13:27:54 +0000

    Tests

--- src/test/java/ie/atu/week8/projectexercise/ProductServiceTest.java
+++ src/test/java/ie/atu/week8/projectexercise/ProductServiceTest.java
@@ -3,15 +3,24 @@ package ie.atu.week8.projectexercise;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
-    @Autowired
+    @InjectMocks
     private ProductService productService;
-    @Autowired
+    @Mock
     private ProductRepository productRepository;
 
     @BeforeEach
@@ -22,7 +31,26 @@ class ProductServiceTest {
     void testSaveProductForApplyDiscount()
     {
         Product product = new Product(1L, "Laptop", "Expensive", 1500);
+        when(productRepository.save(product)).thenReturn(product);
         Product expectedProduct = productService.saveProduct(product);
         assertEquals(1350, expectedProduct.getPrice());
+    }
+
+    @Test
+    void testValidateProductForIllegalArgument()
+    {
+        Product product = new Product(1L, "Laptop", "Expensive", -1500);
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, ()-> productService.saveProduct(product));
+        assertEquals("Price cannot be negative", iae.getMessage());
+    }
+
+
+    @Test
+    void testGetProductById() {
+        Product product = new Product(1L, "Product 1", "Description", 100.0);
+        when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+        Product result = productService.getProductById(1L).orElse(null);
+        assertNotNull(result);
+        assertEquals("Product 1", result.getName());
     }
 }
